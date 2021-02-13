f3cbe2d35c71d76ac674e8b49627a848e7c0cdf9
Alexander Brevig <alexanderbrevig@gmail.com>
2021-02-13 02:45:30 +0100

    Return in from main

--- shellcoderun.c
+++ shellcoderun.c
@@ -23,7 +23,7 @@ char code64[] =
 unsigned char code[] = \
 	"\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80";
 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc < 2) {
 		printf("usage: play 'shellcode'");
